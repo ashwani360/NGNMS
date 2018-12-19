287ba13e5a91560268d975811e68e5309943a211
ashwanis <ashwanis@360NDCLP119.mshome.net>
2018-12-20 03:40:06 +0530

    dasd

--- NGAutomationFinal/src/Testscript/CeaseOrder.java
+++ NGAutomationFinal/src/Testscript/CeaseOrder.java
@@ -1,68 +1,62 @@
 package Testscript;
 
+import java.lang.reflect.Method;
+
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
+
+import com.relevantcodes.extentreports.LogStatus;
 
 import Driver.DataReader;
 import Driver.DriverTestcase;
 import Reporter.ExtentTestManager;
 
+
 public class CeaseOrder extends DriverTestcase {	
-		
-	@Test(dataProviderClass=DataReader.class,dataProvider="InputData")
-		public void CreateAndProcess(Object[] Data) throws Exception
-		{
-		if(Data[0].equals("Cease")){		
+
+@AfterMethod
+public void returntoAccountPages(Method method,ITestResult result) throws Exception
+{
+	if(method.getName().equals("CeaseAndProcess")){
+	CeaseOrderhelper.GotoErrors();
+	String base64Screenshot="data:image/png;base64,"+((TakesScreenshot)dr).getScreenshotAs(OutputType.BASE64);
+	ExtentTestManager.getTest().log(LogStatus.FAIL,"Test Failed",ExtentTestManager.getTest().addBase64ScreenShot(base64Screenshot)); 
+	NavigationHelper.GotoDocument();
+	NavigationHelper.GotoAccount();
+	//itr=itr+1;
+	}
+}
+	
+@Test(dataProviderClass=DataReader.class,dataProvider="CeaseOrder")
+	public void CeaseAndProcess(Object[] Data) throws Exception
+	{
+	if(Data[0].toString().equalsIgnoreCase("Cease")){		
 
 		CeaseOrderhelper.CreatCeaseOrder(Data);
-//	1. Navigate to Accounts Composite Order page and click the order which you want to Disconnect.
-//	2. In the screen under Instance field pick the Instance id of Ethernet Connection product order and move to main composite order page.
-//	3. In the screen navigate to Product Instances tab and search for the Instance id.
-//	4. Now select the instance id and click on Create Disconnect Product Order button.
-//	5. Now navigate to Composite orders page and check for the newly created Disconnected order and click on it.
-//	6. In the screen click on Edit button and update Hard Cease Delay as 0 and Colt Promise Date as yesterdays date and click on Update button.
-		
+		CeaseOrderhelper.EditProduct(Data);
 		CeaseOrderhelper.DecomposeOrder(Data);
-//		6.Once updated go to Accounts order page, search for the newly disconnected order, select it and click on Decompose button.
-//		7. Now 2 End site disconnected product orders should be created.
 		CeaseOrderhelper.ProcessOrder(Data);
-		
-//		8. Navigate back to Accounts order page, select the newly disconnected order and click on Start Processing.
-//		9.Now move to Tasks tab and click on Execution flow link and wait for all tasks to get completed.
-//		10. Meanwhile check the scheduler for any errors.*/
+		CeaseOrderhelper.CompleteOrder(Data);
 		}
 		else
 		{
-			//Code for Hub and Spoke
-			CeaseOrderhelper.CreatCeaseOrder(Data);
-//			1. Navigate to Accounts Composite Order page and click the order which you want to Disconnect.
-//			2. In the screen under Instance field pick the Instance id of Ethernet Connection product order and move to main composite order page.
-//			3. In the screen navigate to Product Instances tab and search for the Instance id.
-//			4. Now select the instance id and click on Create Disconnect Product Order button.
-//			5. Now navigate to Composite orders page and check for the newly created Disconnected order and click on it.
-//			6. In the screen click on Edit button and update Hard Cease Delay as 0 and Colt Promise Date as yesterdays date and click on Update button.
-				
-				CeaseOrderhelper.DecomposeOrder(Data);
-//				6.Once updated go to Accounts order page, search for the newly disconnected order, select it and click on Decompose button.
-//				7. Now 2 End site disconnected product orders should be created.
-				CeaseOrderhelper.ProcessOrder(Data);
-				
-				CeaseOrderhelper.CreatCeaseOrder(Data);
-//				1. Navigate to Accounts Composite Order page and click the order which you want to Disconnect.
-//				2. In the screen under Instance field pick the Instance id of Ethernet Connection product order and move to main composite order page.
-//				3. In the screen navigate to Product Instances tab and search for the Instance id.
-//				4. Now select the instance id and click on Create Disconnect Product Order button.
-//				5. Now navigate to Composite orders page and check for the newly created Disconnected order and click on it.
-//				6. In the screen click on Edit button and update Hard Cease Delay as 0 and Colt Promise Date as yesterdays date and click on Update button.
-					
-					CeaseOrderhelper.DecomposeOrder(Data);
-//					6.Once updated go to Accounts order page, search for the newly disconnected order, select it and click on Decompose button.
-//					7. Now 2 End site disconnected product orders should be created.
-					CeaseOrderhelper.ProcessOrder(Data);
+			CeaseOrderhelper.CreatSpokeCeaseOrder(Data);
+			CeaseOrderhelper.EditProduct(Data);
+			CeaseOrderhelper.DecomposeSpokeOrder(Data);
+			CeaseOrderhelper.ProcessSpokeOrder(Data);
+			CeaseOrderhelper.CompleteSpokeOrder(Data);
+			CeaseOrderhelper.CreatHubCeaseOrder(Data);
+			CeaseOrderhelper.EditHubProduct(Data);
+			CeaseOrderhelper.DecomposeHubOrder(Data);
+			CeaseOrderhelper.ProcessHubOrder(Data);
+			CeaseOrderhelper.CompleteHubOrder(Data);
 		}
-		
-		}
-
 	}
+
+}
 
 
 
