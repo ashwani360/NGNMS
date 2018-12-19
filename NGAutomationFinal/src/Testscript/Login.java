18d144bb371a8e62996cc6c65a04af2c80e38d0b
ashwanis <ashwanis@360NDCLP119.mshome.net>
2018-12-20 03:40:39 +0530

    asda

--- NGAutomationFinal/src/Testscript/Login.java
+++ NGAutomationFinal/src/Testscript/Login.java
@@ -17,8 +17,8 @@ public class Login extends DriverTestcase {
 @Test
 	public void LoginonSiebel() throws Exception
 	{
-		//ExtentTestManager.getTest().setDescription("Login on FaceBook");
-		//Login.SiebelLogin("Siebel");
+		ExtentTestManager.getTest().setDescription("Login on Siebel");
+		Login.SiebelLogin("Siebel");
 		//SiebelLogin.SiebelVerifySuccessLogin("Siebel");
 	}
 
