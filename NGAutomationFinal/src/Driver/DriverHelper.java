1b763b6ea0aa678583d440a255556436e078397a
ashwanis <ashwanis@360NDCLP119.mshome.net>
2018-12-20 04:07:12 +0530

    final

--- NGAutomationFinal/src/Driver/DriverHelper.java
+++ NGAutomationFinal/src/Driver/DriverHelper.java
@@ -48,10 +48,12 @@ public class DriverHelper {
 	public static String Errors;
 	public static String SiebelOrdernumber;
 	public static String ServiceOrder;
+	public static String CircuitReference;
+	
 	public static String CircuitRefnumber;
-    public static String CircuitReference;
-    public static String NetworkReference;
-    public static String TechnicalOrderStatus;
+	public static String NetworkReference;
+	public static String TechnicalOrderStatus;
+	public static String ModifiedCircuitRefnumber;
 	public static String HubCircuitRefNumber;
 	public static String SpokeCircuitRefnumber;
 	public static String ModifiedSiebelOrdernumber;
@@ -61,8 +63,8 @@ public class DriverHelper {
 	{
 		driver=dr;
 		wait = new FluentWait<WebDriver>(dr)       
-				.withTimeout(180, TimeUnit.SECONDS)    
-				.pollingEvery(10, TimeUnit.SECONDS)    
+				.withTimeout(600, TimeUnit.SECONDS)    
+				.pollingEvery(15, TimeUnit.SECONDS)    
 				.ignoring(NoSuchElementException.class); 
 	}
 	 
@@ -109,7 +111,6 @@ public class DriverHelper {
 		Thread.sleep(1000);
 		return el;
 	}
-	
 	public WebElement getwebelement2(final String locator) throws InterruptedException
 	{   
 		if (locator.startsWith("//")|| locator.startsWith("(//"))
@@ -155,6 +156,7 @@ public class DriverHelper {
 			System.out.println("Unable to click on element "+ e.getStackTrace());
 		}
 	}
+	
 	public void switchtofram(WebElement el){
 		driver.switchTo().frame(0);
 		
@@ -163,207 +165,200 @@ public class DriverHelper {
 		driver.switchTo().defaultContent();
 		
 	}
-	public String Getattribute(WebElement el,String attributename) {
-			Log.info(el.getAttribute(attributename));
-			return el.getAttribute(attributename);
+public String Getattribute(WebElement el,String attributename) {
+		Log.info(el.getAttribute(attributename));
+		return el.getAttribute(attributename);
+	}
+public void Moveon(WebElement el) {
+		
+	Actions action = new Actions(driver);
+	 
+    action.moveToElement(el).build().perform();
+	}
+public boolean isElementPresent(String locator) {
+    try {
+        driver.findElement(By.xpath(locator));
+        Log.info("Element Found: True");
+        return true;
+    } catch (NoSuchElementException e) {
+    	 Log.info("Element Found: False");
+        return false;
+    }
+}
+
+public void waitandclickForworkitemsPresent(String locator, int timeout) throws InterruptedException
+{
+		for(int i=0;i<=timeout*60/20;i++){
+			try {
+	            if (isElementPresent(locator)){
+	                break;
+	            }
+	            else{
+	            	//Goto Error Tab
+	            	// Clickon(getwebelement(xml.getlocator("//locators/Tasks/Errors")));
+	            	//if any Error displayed
+	            	//if(isElementPresent("Locator for first error"))
+	            	//{
+	            	//Assert.fail("An Error Occuured on Error Tab");
+	            	//break;
+	            	//}
+	            	//else
+	            	//{
+	            	//Clickon(getwebelement(xml.getlocator("//locators/Tasks/Workitems")));
+	            	//Log.info("Refreshing the Pages");
+		        	//driver.navigate().refresh();
+		        	//Log.info("Waiting For 20 Sec");
+		        	//Thread.sleep(20000);
+	            	//}
+	            	//Assert False and Break
+	            	//else navigate to WorkItems and do the page refresh and weight
+	            	Log.info("Refreshing the Pages");
+		        	driver.navigate().refresh();
+		        	Log.info("Waiting For 20 Sec");
+		        	Thread.sleep(20000);
+	            }
+	            }
+	        catch (Exception e) {
+	        	Log.info(e.getMessage());
+	        }
 		}
-	public void Moveon(WebElement el) {
-			
-		Actions action = new Actions(driver);
-		 
-	    action.moveToElement(el).build().perform();
+}
+
+public void waitandclickForOrderCompleted(String locator, int timeout) throws InterruptedException
+{
+		for(int i=0;i<=timeout*60/20;i++){
+			try {
+	            if (isElementPresent(locator)){
+	                break;
+	            }
+	            else{
+	            	Log.info("Refreshing the Pages");
+		        	driver.navigate().refresh();
+		        	Log.info("Waiting For 20 Sec");
+		        	Thread.sleep(20000);
+	            }
+	            }
+	        catch (Exception e) {
+	        	Log.info(e.getMessage());
+	        }
 		}
-	public boolean isElementPresent(String locator) {
-	    try {
-	        driver.findElement(By.xpath(locator));
-	        Log.info("Element Found: True");
-	        return true;
-	    } catch (NoSuchElementException e) {
-	    	 Log.info("Element Found: False");
-	        return false;
-	    }
-	}
-	
-	public void waitandclickForworkitemsPresent(String locator, int timeout) throws InterruptedException
-	{
-			for(int i=0;i<=timeout*60/20;i++){
-				try {
-		            if (isElementPresent(locator)){
-		                break;
-		            }
-		            else{
-		            	//Goto Error Tab
-		            	// Clickon(getwebelement(xml.getlocator("//locators/Tasks/Errors")));
-		            	//if any Error displayed
-		            	//if(isElementPresent("Locator for first error"))
-		            	//{
-		            	//Assert.fail("An Error Occuured on Error Tab");
-		            	//break;
-		            	//}
-		            	//else
-		            	//{
-		            	//Clickon(getwebelement(xml.getlocator("//locators/Tasks/Workitems")));
-		            	//Log.info("Refreshing the Pages");
-			        	//driver.navigate().refresh();
-			        	//Log.info("Waiting For 20 Sec");
-			        	//Thread.sleep(20000);
-		            	//}
-		            	//Assert False and Break
-		            	//else navigate to WorkItems and do the page refresh and weight
-		            	Log.info("Refreshing the Pages");
-			        	driver.navigate().refresh();
-			        	Log.info("Waiting For 20 Sec");
-			        	Thread.sleep(20000);
-		            }
-		            }
-		        catch (Exception e) {
-		        	Log.info(e.getMessage());
-		        }
-			}
-	}
-	
-	public void waitandclickForOrderCompleted(String locator, int timeout) throws InterruptedException
-	{
-			for(int i=0;i<=timeout*60/20;i++){
-				try {
-		            if (isElementPresent(locator)){
-		                break;
-		            }
-		            else{
-		            	Log.info("Refreshing the Pages");
-			        	driver.navigate().refresh();
-			        	Log.info("Waiting For 20 Sec");
-			        	Thread.sleep(20000);
-		            }
-		            }
-		        catch (Exception e) {
-		        	Log.info(e.getMessage());
-		        }
-			}
-	}
-	
-	public void waitandclickForOrderStarted(String locator, int timeout) throws InterruptedException
-	{
-			for(int i=0;i<=timeout*60/20;i++){
-				try {
-		            if (isElementPresent(locator)){
-		                break;
-		            }
-		            else{
-		            	Log.info("Refreshing the Pages");
-			        	driver.navigate().refresh();
-			        	Log.info("Waiting For 20 Sec");
-			        	Thread.sleep(20000);
-		            }
-		            }
-		        catch (Exception e) {
-		        	Log.info(e.getMessage());
-		        }
-			}
-	}
-	
-	public void waitandForElementDisplay(String locator, int timeout) throws InterruptedException
-	{
-			for(int i=0;i<=timeout*60/20;i++){
-				try {
-		            if (isElementPresent(locator)){
-		                break;
-		            }
-		            else{
-		            	Log.info("Refreshing the Pages");
-			        	driver.navigate().refresh();
-			        	Log.info("Waiting For 20 Sec");
-			        	Thread.sleep(20000);
-		            }
-		            }
-		        catch (Exception e) {
-		        	Log.info(e.getMessage());
-		        }
-			}
-	}
-	
-	public int getwebelementscount(final String locator) throws InterruptedException
-	{ 
-		ellist=driver.findElements(By.xpath(locator));
-		return ellist.size();
-	}
-	
+}
+
+public void waitandclickForOrderStarted(String locator, int timeout) throws InterruptedException
+{
+		for(int i=0;i<=timeout*60/20;i++){
+			try {
+	            if (isElementPresent(locator)){
+	                break;
+	            }
+	            else{
+	            	Log.info("Refreshing the Pages");
+		        	driver.navigate().refresh();
+		        	Log.info("Waiting For 20 Sec");
+		        	Thread.sleep(20000);
+	            }
+	            }
+	        catch (Exception e) {
+	        	Log.info(e.getMessage());
+	        }
+		}
+}
+
+public void waitandForElementDisplay(String locator, int timeout) throws InterruptedException
+{
+		for(int i=0;i<=timeout*60/20;i++){
+			try {
+	            if (isElementPresent(locator)){
+	                break;
+	            }
+	            else{
+	            	Log.info("Refreshing the Pages");
+		        	driver.navigate().refresh();
+		        	Log.info("Waiting For 20 Sec");
+		        	Thread.sleep(20000);
+	            }
+	            }
+	        catch (Exception e) {
+	        	Log.info(e.getMessage());
+	        }
+		}
+}
+
+public int getwebelementscount(final String locator) throws InterruptedException
+{ 
+	ellist=driver.findElements(By.xpath(locator));
+	return ellist.size();
+}
+
 	public void SendKeys(WebElement el,String value) {
 		el.sendKeys(value);
 	}
-
 	public void SendkeaboardKeys(WebElement el,Keys k) {
 		el.sendKeys(k);
 	}
 	
+	
 	public String GetText(WebElement el) {
-			String actual=el.getText().toUpperCase().toString();
-	//		String actual1=el.getText().toUpperCase().toString();
-			return actual;
-		}
+		String actual=el.getText().toLowerCase().toString();
+		return actual;
+	}
 	
 	public String GetInputValue(WebElement el) {
 		String actual=el.getAttribute("value");
 		return actual;
 		}
-
-	public String Getkeyvalue(String Key) throws IOException{ 
-			PropertyReader pr=new PropertyReader();
-		    String Keyvalue;
-			Keyvalue=pr.readproperty(Key);
+	
+	public String Getkeyvalue(String Key) throws IOException
+			{ PropertyReader pr=new PropertyReader();
+		      String Keyvalue;
+			 Keyvalue=pr.readproperty(Key);
+		
 			return Keyvalue;
-		}
-	
+			}
 	public void VerifyTextpresent(String text) throws IOException
-		{ 
-			Log.info(text);
-			Assert.assertFalse(driver.findElement(By.xpath("//*[text()='"+text+"']")).isDisplayed());
-		}
-	
+	{ 
+		Log.info(text);
+		Assert.assertFalse(driver.findElement(By.xpath("//*[text()='"+text+"']")).isDisplayed());
+	}
 	public void VerifyText(String text) throws IOException
-		{ 
-			Log.info(text);
-			Assert.assertTrue(driver.findElement(By.xpath("//*[text()='"+text+"']")).isDisplayed());
-		}
-	
+	{ 
+		Log.info(text);
+		Assert.assertTrue(driver.findElement(By.xpath("//*[text()='"+text+"']")).isDisplayed());
+	}
 	public String Gettext(WebElement el) throws IOException
-		{ 
-			String text=el.getText().toString();
-			return text;
-		}
-	
+	{ 
+		String text=el.getText().toString();
+		return text;
+	}
 	public String[] GetText2(WebElement el) throws IOException
-		{ 
-			String text=el.getText().toString();
-			String[] text2=text.split(" \\[");
-			Log.info("New Task Name is:"+text2);
-			return text2;
-		}
-	
-	public String GetText3(WebElement el, String string) throws IOException
-		{ 
-			String text=el.getText().toString();
-			return text;
-		}
-	
+	{ 
+		String text=el.getText().toString();
+		String[] text2=text.split(" \\[");
+		Log.info("New Task Name is:"+text2);
+		return text2;
+	}
 	public void Select(WebElement el, String value) throws IOException
-		{ 
-			Select s1=new Select(el);
-			s1.selectByVisibleText(value);
-		}
-	
+	{ 
+		Select s1=new Select(el);
+		s1.selectByVisibleText(value);
+		
+	}
 	public void Clear(WebElement el) throws IOException
-		{ 
-			el.clear();
-		}
-	
+	{ 
+		el.clear();
+		
+	}
 	public void AcceptJavaScriptMethod(){
-			Alert alert = driver.switchTo().alert();
-			alert.accept();
-		}
+		
+		Alert alert = driver.switchTo().alert();
+		
+		alert.accept();
+		
+	}
 	
 	public void savePage(){
-			Actions keyAction = new Actions(driver);     
-			keyAction.keyDown(Keys.CONTROL).sendKeys("s").keyUp(Keys.CONTROL).perform();
-		}
+		Actions keyAction = new Actions(driver);     
+		keyAction.keyDown(Keys.CONTROL).sendKeys("s").keyUp(Keys.CONTROL).perform();
 	}
+	
+}
