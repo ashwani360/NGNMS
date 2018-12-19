01c5e36b4aeff15750ff5dd1af46884e65e7bbdf
ashwanis <ashwanis@360NDCLP119.mshome.net>
2018-12-20 03:33:49 +0530

    latets

--- NGAutomationFinal/src/Testscript/SiebelOrder.java
+++ NGAutomationFinal/src/Testscript/SiebelOrder.java
@@ -1,24 +1,76 @@
 package Testscript;
+import java.lang.reflect.Method;
+
 import org.testng.annotations.Test;
+import org.testng.annotations.AfterMethod;
 
 import Driver.DataReader;
 import Driver.DriverTestcase;
-import Reporter.ExtentTestManager;
 
 
 public class SiebelOrder extends DriverTestcase {	
-	
-@Test(dataProviderClass=DataReader.class,dataProvider="SiebelData")
-	public void CreateOrder(Object[] Inputdata) throws Exception
+
+	@AfterMethod
+	public void returntoAccountPages(Method method) throws Exception
 	{
-		
-//	1. Go to Accounts and enter the OCN number as 4159381 and click Enter.
-//	2. Click on the name and Move to Install Assets and select New order.
-//	3. Enter the Red marked fields like Opportunity, Request Received date, Delivery channel - WLC, Ordering Party, Ordering Party address, 
-//	Ordering Party contract and Sales Channel - Direct.
-//	4. Click on New Service Order.
-	SiebelOrderhelper.CreateSiebelOrder(Inputdata);
-	SiebelOrderhelper.AddProduct(Inputdata);
+		if(method.getName().equals("CreateP2POrder")){
+// Code to Logout once order is created
+			Login.SiebelLogout("Siebel");
+		}
+	}
+	
+@Test(dataProviderClass=DataReader.class,dataProvider="SiebelOrder")
+
+public void CreateNewOrder(Object[] data) throws Exception{
+if(data[2].toString().equals("P2P"))	{
+	SiebelOrderhelper.CreateSiebelOrder(data);
+	SiebelOrderhelper.AddEthernetLineSiteA(data);
+	SiebelOrderhelper.AddEthernetLineSiteBAddress(data);
+	SiebelOrderhelper.ASiteCustomize(data);
+	SiebelOrderhelper.BSiteCustomize(data);
+	SiebelOrderhelper.Workflow(data);
+	}
+else if(data[2].toString().equals("HUB"))
+	{
+	SiebelOrderhelper.CreateSiebelOrder(data);
+	SiebelOrderhelper.AddProductEthernetHub(data);
+	}
+else if(data[2].toString().equals("Spoke"))
+	{
+	SiebelOrderhelper.CreateSiebelOrder(data);
+	SiebelOrderhelper.AddProductEthernetSpoke(data);
+	}
+}
+
+@Test
+	public void CompleteP2POrder() throws Exception
+	{
+	SiebelOrderhelper.OrderCompleteEthernetLine();
 	}
 
+@Test
+	public void CreateHubOrder(Object[] data) throws Exception
+	{
+		SiebelOrderhelper.CreateSiebelOrder(data);
+		SiebelOrderhelper.AddProductEthernetHub(data);
+	}
+
+@Test
+	public void CompleteHubOrder() throws Exception
+	{
+		SiebelOrderhelper.OrderCompleteEthernetHub();
+	}
+
+@Test
+	public void CreateSpokeOrder(Object[] data) throws Exception
+	{
+		SiebelOrderhelper.CreateSiebelOrder(data);
+		SiebelOrderhelper.AddProductEthernetSpoke(data);
+	}
+	
+@Test
+	public void CompleteSpokeOrder() throws Exception
+	{
+		SiebelOrderhelper.OrderCompleteEthernetLine();
+	}
 }
