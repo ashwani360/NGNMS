61e40c3bd291da41189c38078846ff180fd414e5
ashwanis <ashwanis@360NDCLP119.mshome.net>
2018-12-20 06:25:04 +0530

    bj

--- NGAutomationFinal/src/Testscript/SiebelOrder.java
+++ NGAutomationFinal/src/Testscript/SiebelOrder.java
@@ -1,8 +1,5 @@
 package Testscript;
-import java.lang.reflect.Method;
-
 import org.testng.annotations.Test;
-import org.testng.annotations.AfterMethod;
 
 import Driver.DataReader;
 import Driver.DriverTestcase;
@@ -10,67 +7,28 @@ import Driver.DriverTestcase;
 
 public class SiebelOrder extends DriverTestcase {	
 
-	@AfterMethod
-	public void returntoAccountPages(Method method) throws Exception
-	{
-		if(method.getName().equals("CreateP2POrder")){
-// Code to Logout once order is created
-			Login.SiebelLogout("Siebel");
+
+
+@Test(dataProviderClass=DataReader.class,dataProvider="Siebeldata")
+	public void CreateNewOrder(Object[] data) throws Exception{
+	if(data[35+2].toString().equals("P2P"))	{
+		SiebelOrderhelper.CreateSiebelOrder(data);
+		SiebelOrderhelper.AddEthernetLineSiteA(data);
+		SiebelOrderhelper.AddEthernetLineSiteBAddress(data);
+		SiebelOrderhelper.ASiteCustomize(data);
+		SiebelOrderhelper.BSiteCustomize(data);
+		SiebelOrderhelper.Workflow(data);
 		}
-	}
-	
-@Test(dataProviderClass=DataReader.class,dataProvider="SiebelOrder")
-
-public void CreateNewOrder(Object[] data) throws Exception{
-if(data[2].toString().equals("P2P"))	{
-	SiebelOrderhelper.CreateSiebelOrder(data);
-	SiebelOrderhelper.AddEthernetLineSiteA(data);
-	SiebelOrderhelper.AddEthernetLineSiteBAddress(data);
-	SiebelOrderhelper.ASiteCustomize(data);
-	SiebelOrderhelper.BSiteCustomize(data);
-	SiebelOrderhelper.Workflow(data);
-	}
-else if(data[2].toString().equals("HUB"))
-	{
-	SiebelOrderhelper.CreateSiebelOrder(data);
-	SiebelOrderhelper.AddProductEthernetHub(data);
-	}
-else if(data[2].toString().equals("Spoke"))
-	{
-	SiebelOrderhelper.CreateSiebelOrder(data);
-	SiebelOrderhelper.AddProductEthernetSpoke(data);
-	}
-}
-
-@Test
-	public void CompleteP2POrder() throws Exception
-	{
-	SiebelOrderhelper.OrderCompleteEthernetLine();
-	}
-
-@Test
-	public void CreateHubOrder(Object[] data) throws Exception
-	{
+	else if(data[35+2].toString().equals("HUB"))
+		{
 		SiebelOrderhelper.CreateSiebelOrder(data);
 		SiebelOrderhelper.AddProductEthernetHub(data);
-	}
+		}
+	else if(data[35+2].toString().equals("Spoke"))
+		{
 
-@Test
-	public void CompleteHubOrder() throws Exception
-	{
-		SiebelOrderhelper.OrderCompleteEthernetHub();
-	}
-
-@Test
-	public void CreateSpokeOrder(Object[] data) throws Exception
-	{
 		SiebelOrderhelper.CreateSiebelOrder(data);
 		SiebelOrderhelper.AddProductEthernetSpoke(data);
-	}
-	
-@Test
-	public void CompleteSpokeOrder() throws Exception
-	{
-		SiebelOrderhelper.OrderCompleteEthernetLine();
-	}
+		}
+}
 }
